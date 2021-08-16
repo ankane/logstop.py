ea5d43dbd5cf72f3beb5eb2d36ce51d6227ce5db
Andrew Kane <andrew@ankane.org>
2021-08-16 00:08:59 -0700

    Version bump to 0.1.1 [skip ci]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='logstop',
-    version='0.1.0',
+    version='0.1.1',
     description='Keep personal data out of your logs',
     url='https://github.com/ankane/logstop.py',
     author='Andrew Kane',
