43322f11243ea43b9d6804ffa18c1cc655791f3d
Andrew Kane <andrew@ankane.org>
2021-10-15 20:02:31 -0700

    Added long description [skip ci]

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name='logstop',
     version='0.1.1',
     description='Keep personal data out of your logs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/ankane/logstop.py',
     author='Andrew Kane',
     author_email='andrew@ankane.org',
